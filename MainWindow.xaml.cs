f38c7cfbe552ce0fdfb0e18ddfbc45619ecda677
zuzan <zuzan@DESKTOP-PR8MS15>
2019-12-06 02:21:27 +0100

    Naprawione kwadraciki :)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -28,21 +28,12 @@ namespace Ha {
             rows = Convert.ToInt32(rowTb.Text);
             cols = Convert.ToInt32(colTb.Text);
 
-
-            if (canvas.Width / cols < canvas.ActualHeight / rows) {
-                if ((int)canvas.Width / cols % 2 == 0) {
-                    step = (int)canvas.Width / cols;
-                } else {
-                    step = (int)canvas.Width / cols - 1;
-                }
+            if (canvas.Width / cols < canvas.Height / rows) {
+                step = (int)canvas.Width / cols;
             } else {
-                if ((int)canvas.Height / rows % 2 == 0) {
-                    step = (int)canvas.Height / rows;
-                } else {
-                    step = (int)canvas.Height / rows - 1;
-                }
+                step = (int)canvas.Height / rows;
             }
-
+            Console.WriteLine("Step: " + step);
             offsetX = (canvas.Width - step * cols) / 2;
             offsetY = (canvas.Height - step * rows) / 2;
 
@@ -52,13 +43,12 @@ namespace Ha {
 
                     X1 = i * step + offsetX,
                     Y1 = 0 + offsetY,
-                    
+
                     X2 = i * step + offsetX,
                     Y2 = rows * step + offsetY
                 };
-                Console.WriteLine("("+lineX.X1+","+lineX.Y1+")");
+                Console.WriteLine("(" + lineX.X1 + "," + lineX.Y1 + ")");
                 canvas.Children.Add(lineX);
-
             }
 
             for (int j = 0; j < rows + 1; j++) {
@@ -73,7 +63,6 @@ namespace Ha {
                 };
 
                 canvas.Children.Add(lineY);
-
             }
         }
 
@@ -83,17 +72,43 @@ namespace Ha {
             if (startPoint.X > offsetX && startPoint.X < cols * step + offsetX &&
                 startPoint.Y > offsetY && startPoint.Y < rows * step + offsetY) {
 
-                Console.WriteLine((int)(startPoint.X) / step * step+offsetX);
-                Console.WriteLine((int)(startPoint.Y) / step * step+offsetY);
+                //Console.WriteLine((int)(startPoint.X) / step * step + offsetX);
+                // Console.WriteLine((int)(startPoint.Y) / step * step + offsetY);
 
                 Rectangle rect = new Rectangle {
                     Fill = Brushes.Black,
                     Height = step,
                     Width = step
+
                 };
-                Canvas.SetLeft(rect, (int)(startPoint.X) / step * step+offsetX);
-                Canvas.SetTop(rect, (int)(startPoint.Y) / step * step+offsetY);
+                Point converted = new Point();
+                converted.X = startPoint.X - offsetX;
+                converted.Y = startPoint.Y - offsetY;
+                int c, r;
+
+                if (converted.X % step < step) {
+                    c = (int)converted.X / step + 1;
+                } else {
+                    c = (int)converted.X / step;
+                }
+
+                if (converted.Y % step < step) {
+                    r = (int)converted.Y / step + 1;
+                } else {
+                    r = (int)converted.Y / step;
+                }
+
+                c = c - 1;
+                r = r - 1;
+
+                Canvas.SetLeft(rect, c * step + offsetX);
+                Canvas.SetTop(rect, r * step + offsetY);
                 canvas.Children.Add(rect);
+                Console.WriteLine("( " + startPoint.X + ", " + startPoint.Y + ")");
+
+                Console.WriteLine("X c*step + offsetX \t" + (c * step + offsetX));
+                Console.WriteLine("Y r*step + offsetY\t" + (r * step + offsetY));
+
             }
         }
     }
