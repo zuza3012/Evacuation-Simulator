a5ef0eb54d371650d0896fcc04571868b53d7966
zuzan <zuzan@DESKTOP-PR8MS15>
2019-12-06 17:08:20 +0100

    Dodawanie przeszkód, osób i drzwi.

---
+++ Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ha {
+    class Cell {
+        public double x, y;
+        public bool isAWall = false, isAPerson = false, isADoor = false;
+
+        public Cell(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Cell() { }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,9 +12,26 @@ namespace Ha {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+
         }
         int rows, cols, step;
         double offsetX, offsetY;
+        Cell[][] cells;
+
+        private void checkObst(object sender, RoutedEventArgs e) {
+            door.IsChecked = false;
+            people.IsChecked = false;
+        }
+
+        private void checkDoor(object sender, RoutedEventArgs e) {
+            obst.IsChecked = false;
+            people.IsChecked = false;
+        }
+
+        private void checkPeople(object sender, RoutedEventArgs e) {
+            door.IsChecked = false;
+            obst.IsChecked = false;
+        }
 
         protected override void OnClosed(EventArgs e) {
             base.OnClosed(e);
@@ -28,6 +45,7 @@ namespace Ha {
             rows = Convert.ToInt32(rowTb.Text);
             cols = Convert.ToInt32(colTb.Text);
 
+
             if (canvas.Width / cols < canvas.Height / rows) {
                 step = (int)canvas.Width / cols;
             } else {
@@ -37,6 +55,14 @@ namespace Ha {
             offsetX = (canvas.Width - step * cols) / 2;
             offsetY = (canvas.Height - step * rows) / 2;
 
+            cells = new Cell[cols][];
+
+            for (int i = 0; i < cols; i++) {
+                cells[i] = new Cell[rows];
+                for (int j = 0; j < rows; j++) {
+                    cells[i][j] = new Cell(offsetX + i * step, offsetY + j * step);
+                }
+            }
 
             for (int i = 0; i < cols + 1; i++) {
                 Line lineX = new Line {
@@ -70,47 +96,88 @@ namespace Ha {
         private void DrawRect(object sender, MouseButtonEventArgs e) {
 
             Point startPoint = e.GetPosition(canvas);
-            if (startPoint.X > offsetX && startPoint.X < cols * step + offsetX &&
-                startPoint.Y > offsetY && startPoint.Y < rows * step + offsetY) {
 
-                //Console.WriteLine((int)(startPoint.X) / step * step + offsetX);
-                // Console.WriteLine((int)(startPoint.Y) / step * step + offsetY);
+            Point converted = new Point {
+                X = startPoint.X - offsetX,
+                Y = startPoint.Y - offsetY
+            };
+            int c, r;
 
-                Rectangle rect = new Rectangle {
-                    Fill = Brushes.Black,
-                    Height = step,
-                    Width = step
+            c = (int)converted.X / step;
+            r = (int)converted.Y / step;
 
-                };
-                Point converted = new Point();
-                converted.X = startPoint.X - offsetX;
-                converted.Y = startPoint.Y - offsetY;
-                int c, r;
+            Rectangle rect = new Rectangle();
 
-                if (converted.X % step < step) {
-                    c = (int)converted.X / step + 1;
-                } else {
-                    c = (int)converted.X / step;
+            if (startPoint.X > offsetX + step && startPoint.X < cols * step + offsetX - step &&
+                startPoint.Y > offsetY + step && startPoint.Y < rows * step + offsetY - step) {
+
+                if (obst.IsChecked == true) {
+                    rect.Fill = Brushes.DarkGray;
+                    if (cells[c][r].isAWall) {
+                        cells[c][r].isAWall = false;
+                        rect.Width = step - 2;
+                        rect.Height = step - 2;
+                        rect.Fill = Brushes.White;
+                        Canvas.SetLeft(rect, c * step + offsetX + 1);
+                        Canvas.SetTop(rect, r * step + offsetY + 1);
+                    } else {
+                        cells[c][r].isAWall = true;
+                        rect.Width = step - 4;
+                        rect.Height = step - 4;
+                        rect.Fill = Brushes.DarkGray;
+                        Canvas.SetLeft(rect, c * step + offsetX + 2);
+                        Canvas.SetTop(rect, r * step + offsetY + 2);
+                    }
+                    canvas.Children.Add(rect);
+
+                } else if (people.IsChecked == true) {
+                    Ellipse ellipse = new Ellipse();
+                    if (cells[c][r].isAPerson) {
+                        cells[c][r].isAPerson = false;
+                        ellipse.Width = step - 2;
+                        ellipse.Height = step - 2;
+                        ellipse.Stroke = Brushes.White;
+                        ellipse.StrokeThickness = 4;
+                        Canvas.SetLeft(ellipse, c * step + offsetX + 1);
+                        Canvas.SetTop(ellipse, r * step + offsetY + 1);
+                    } else {
+                        cells[c][r].isAPerson = true;
+                        ellipse.Width = step - 4;
+                        ellipse.Height = step - 4;
+                        ellipse.Stroke = Brushes.Black;
+                        ellipse.StrokeThickness = 2;
+                        Canvas.SetLeft(ellipse, c * step + offsetX + 2);
+                        Canvas.SetTop(ellipse, r * step + offsetY + 2);
+                    }
+
+                    canvas.Children.Add(ellipse);
                 }
+            }
+            if ((startPoint.X > offsetX && startPoint.X < offsetX + cols * step && startPoint.Y > offsetY && startPoint.Y < offsetY + cols * step) 
+                && !(startPoint.X > offsetX + step && startPoint.X < cols * step + offsetX - step &&
+                startPoint.Y > offsetY + step && startPoint.Y < rows * step + offsetY - step)) {
 
-                if (converted.Y % step < step) {
-                    r = (int)converted.Y / step + 1;
-                } else {
-                    r = (int)converted.Y / step;
+                if (door.IsChecked == true) {
+                    rect.Fill = Brushes.Red;
+                    if (cells[c][r].isADoor) {
+                        cells[c][r].isADoor = false;
+                        rect.Width = step - 4;
+                        rect.Height = step - 4;
+                        rect.Fill = Brushes.DarkGray;
+                        Canvas.SetLeft(rect, c * step + offsetX + 2);
+                        Canvas.SetTop(rect, r * step + offsetY + 2);
+                    } else {
+                        cells[c][r].isADoor = true;
+                        rect.Width = step - 4;
+                        rect.Height = step - 4;
+                        rect.Fill = Brushes.Red;
+                        Canvas.SetLeft(rect, c * step + offsetX + 2);
+                        Canvas.SetTop(rect, r * step + offsetY + 2);
+                    }
+                    canvas.Children.Add(rect);
                 }
-
-                c = c - 1;
-                r = r - 1;
-
-                Canvas.SetLeft(rect, c * step + offsetX);
-                Canvas.SetTop(rect, r * step + offsetY);
-                canvas.Children.Add(rect);
-                Console.WriteLine("( " + startPoint.X + ", " + startPoint.Y + ")");
-
-                Console.WriteLine("X c*step + offsetX \t" + (c * step + offsetX));
-                Console.WriteLine("Y r*step + offsetY\t" + (r * step + offsetY));
-
             }
         }
     }
 }
+
