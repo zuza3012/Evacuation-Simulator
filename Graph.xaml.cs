735f05aeb27b89e6d8b7662fc5ed4668a29e5eaa
zuzan <zuzan@DESKTOP-PR8MS15>
2020-01-05 18:15:44 +0100

    Dodanie ikony i okna dialogowego do zapisu zdjęcia

--- Graph.xaml.cs
+++ Graph.xaml.cs
@@ -3,9 +3,6 @@ using LiveCharts.Defaults;
 using LiveCharts.Wpf;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.Drawing.Printing;
 using System.Globalization;
 using System.IO;
 using System.Linq;
@@ -56,20 +53,27 @@ namespace Ha {
 
         private void SaveGraph_Click(object sender, RoutedEventArgs e) {
 
-             int chartWidth = (int)chart1.ActualWidth;
-             int chartHeight = (int)chart1.ActualHeight;
-             double resolution = 96d;
+            int chartWidth = (int)chart1.ActualWidth;
+            int chartHeight = (int)chart1.ActualHeight;
+            double resolution = 96d;
 
-             FileStream stream = new FileStream("graph.png", FileMode.Create);
-             RenderTargetBitmap bmp = new RenderTargetBitmap(chartWidth, chartHeight, resolution, resolution, PixelFormats.Pbgra32);
-             bmp.Render(chart1);
+            string fileName = DateTime.Now.ToString("h/mm/ss_tt");
 
-             BmpBitmapEncoder encoder = new BmpBitmapEncoder();
-             encoder.Frames.Add(BitmapFrame.Create(bmp));
-             encoder.Save(stream);
+            System.Windows.Forms.FolderBrowserDialog openfiledalog = new System.Windows.Forms.FolderBrowserDialog();
+            if (openfiledalog.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
 
-            MessageBox.Show("Graph has been saved!", "Saving inforamtion");
+                var dest = Path.Combine(openfiledalog.SelectedPath, "graph_" + fileName + ".png");
 
+                FileStream stream = new FileStream(dest, FileMode.Create);
+                RenderTargetBitmap bmp = new RenderTargetBitmap(chartWidth, chartHeight, resolution, resolution, PixelFormats.Pbgra32);
+                bmp.Render(chart1);
+
+                BmpBitmapEncoder encoder = new BmpBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(bmp));
+                encoder.Save(stream);
+
+                MessageBox.Show("Graph has been saved!", "Saving inforamtion");
+            }
         }
 
         private void DrawChart(CartesianChart chart, string filePath, string chartTitle, string xTitle, string yTitle) {
@@ -90,7 +94,7 @@ namespace Ha {
                      PointGeometry = null, // DefaultGeometries.Square tylko jak jest malo punktuf, inzcaej kupcia
                      Title = chartTitle,
                      Fill = System.Windows.Media.Brushes.White
-                     
+
                    }
              };
 
